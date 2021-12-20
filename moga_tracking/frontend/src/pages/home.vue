634d8005665a0e62ca2131d731f0b2daf198ae7c
Rainforest Cheng <rainforestnick@gmail.com>
2021-12-20 13:55:34 +0800

    feat: vite + tailwindcss + daisyui (draft)

---
+++ moga_tracking/frontend/src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ JSON.stringify(data) }}</div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const data = ref<MessageEvent>();
+    const connection = new WebSocket("ws://localhost:3000/ws");
+    connection.onmessage = (event) => {
+      console.log(event);
+      data.value = event.data;
+    };
+    return { data };
+  },
+});
+</script>
