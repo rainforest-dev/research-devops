a6bb7c7de4f5b54b5a27a5072e67a1df44d17b6e
Rainforest <r08521610@ntu.edu.tw>
2022-02-07 11:13:41 +0000

    chore: setup backend devcontainer

--- moga_tracking/backend/moga_tracking/routers/nacre.py
+++ moga_tracking/backend/moga_tracking/routers/nacre.py
@@ -1,5 +1,6 @@
 import os
 from typing import Optional
+import tempfile
 from dotenv import load_dotenv
 from fastapi import APIRouter
 import numpy as np
@@ -7,7 +8,6 @@ from research_data.utils import paramsToImg
 from starlette.background import BackgroundTasks
 from starlette.responses import FileResponse
 from PIL import Image
-import tempfile
 
 load_dotenv()
 
@@ -20,22 +20,32 @@ async def nacre_image(background_tasks: BackgroundTasks,
                       img_size: Optional[int] = 512,
                       format: str = 'jpg',
                       unit_cell: bool = False):
-  params = [int(item) for item in params.split('_')]
-  img = paramsToImg(param=params, is_unit_cell=unit_cell, chromosome_length=img_size)
-  filename = f'{"".join(str(item) for item in params)}'
-  # [reference](https://stackoverflow.com/a/64717120)
-  _, path = tempfile.mkstemp(suffix=f'.{format}')
-  if format == 'jpg' or format == 'png':
-    img = Image.fromarray(img * 255)
-    img.save(path)
-  elif format == 'npy':
-    # !currently broken
-    np.save(path, img)
+    """return nacre image
 
-  background_tasks.add_task(lambda path: os.unlink(path), path)
-  return FileResponse(path=path, filename=f'{filename}.{format}')
+    Args:
+        background_tasks (BackgroundTasks):
+        params (str): design params
+        img_size (Optional[int], optional): Defaults to 512.
+        format (str, optional): Defaults to 'jpg'.
+        unit_cell (bool, optional): Defaults to False.
+    """
+    params = [int(item) for item in params.split('_')]
+    img = paramsToImg(params=params, is_unit_cell=unit_cell,
+                      chromosome_length=img_size)
+    filename = f'{"".join(str(item) for item in params)}'
+    # [reference](https://stackoverflow.com/a/64717120)
+    _, path = tempfile.mkstemp(suffix=f'.{format}')
+    if format == 'jpg' or format == 'png':
+        img = Image.fromarray(img * 255)
+        img.save(path)
+    elif format == 'npy':
+        # !currently broken
+        np.save(path, img)
+
+    background_tasks.add_task(os.unlink, path)
+    return FileResponse(path=path, filename=f'{filename}.{format}')
 
 
-@router.get('/design_space/')
-async def nacre_image(params: str):
-  pass
+# @router.get('/design_space/')
+# async def nacre_image(params: str):
+#     pass
