1316aa13b4a95e5b458cccafe9581ce10bae74ce
Rainforest <r08521610@ntu.edu.tw>
2022-02-09 18:06:18 +0000

    feat: add export and import yaml

--- moga_tracking/frontend/src/pages/design_space/index.vue
+++ moga_tracking/frontend/src/pages/design_space/index.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
 .grid.grid-cols-10.full.gap-4
-  .dropdown.dropdown-hover(v-for="item in items" :key="item.id" @click="handleClick(item.id)" :class="[item.disabled && 'brightness-50']")
-    .grid.grid-cols-3.cursor-pointer
+  .dropdown.dropdown-hover(v-for="item in items" :key="item.id" @click="handleClick(item.id)")
+    .grid.grid-cols-3.cursor-pointer(:class="[item.disabled && 'brightness-50']")
       .aspect-square.border.border-neutral(v-for="cell, index in item.parameters" :key="`${item.id}-${index}`" :class="[cell === 0 ? 'bg-black' : 'bg-white']")
     .dropdown-content.bg-base-100.p-4.w-96.shadow-xl.border.card
       img.aspect-square.border(:src="item.src")
+.fixed.right-0.bottom-0.card
+  .card-body
+    .card-actions
+      label.btn Import
+        input.hidden(type="file" @change="importYAML"  accept="yaml")
+      button.btn.btn-primary(@click="exportYAML") Export
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
 import { classToPlain, deserializeArray, Expose, plainToClass } from "class-transformer";
 import "lodash.product";
 // @ts-ignore
 import { product } from "lodash";
+import { parse, stringify } from "yaml"
 import { useStorage } from "@vueuse/core";
 import { designSpace } from "@/utils/constants";
-import { id } from "@/utils";
+import { download, id, readFile } from "@/utils";
 
 class Item {
   parameters!: number[];
@@ -56,4 +62,24 @@ const handleClick = (id: string) => {
     }),
   ];
 };
+
+const importYAML = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  const files = target.files
+  if (!files || !files[0]) return
+  readFile(files[0], (text) => {
+    const data = parse(text) as { S: { [key: string]: number[] } }
+    const ids = Object.keys(data.S)
+    items.value = [
+      ...items.value.map((e) => {
+        e.disabled = !(ids.includes(e.id))
+        return e;
+      }),
+    ];
+  })
+}
+
+const exportYAML = () => {
+  download(stringify({ S: Object.fromEntries(items.value.filter(e => !e.disabled).map(e => [e.id, e.parameters])) }), 'export.yaml', 'text/yaml')
+}
 </script>
--- moga_tracking/frontend/src/utils/index.ts
+++ moga_tracking/frontend/src/utils/index.ts
@@ -1,1 +1,24 @@
 export const id = (list: number[]) => list.join("_");
+
+export function readFile(file: File, callback: (text: string) => void) {
+  const reader = new FileReader();
+  reader.onload = async (e) => {
+    const text = e.target?.result;
+    try {
+      callback(text?.toString()!);
+    } catch (error) {}
+  };
+  reader.readAsText(file);
+}
+
+export function download(
+  content: BlobPart,
+  fileName: string,
+  contentType: string
+) {
+  var a = document.createElement("a");
+  var file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
