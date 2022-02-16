7f10763878e743337ce2de7cb3bfe94a104d5fd4
Rainforest <r08521610@ntu.edu.tw>
2022-02-16 15:58:27 +0000

    refactor(moga_tracking): improve the ux of nacre preview

--- moga_tracking/frontend/src/types/form.ts
+++ moga_tracking/frontend/src/types/form.ts
@@ -1,11 +1,13 @@
-export enum FieldType {
+enum FieldType {
   String,
   Boolean,
   Number,
 }
 
-export interface Field<T = string> {
+interface Field<T = string> {
   name: T;
   type: FieldType;
   validation?: () => boolean;
 }
+
+export { Field, FieldType };
