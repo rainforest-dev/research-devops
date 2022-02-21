fc5c2e87b3ee099c2517a3ce96c4ad7991ecd9df
Rainforest <r08521610@ntu.edu.tw>
2022-02-21 06:46:51 +0000

    feat(moga_tracking): add docs pages

---
+++ moga_tracking/frontend/src/pages/docs.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.full.overflow-auto
+  iframe.full.border(:src="url")
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const url = computed(() => (Array.isArray(route.query.url) ? route.query.url[0] : route.query.url) as string)
+</script>
